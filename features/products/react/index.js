3ed8a8222b5d5f83cf9c7292d94d9acf464b3849
Nicolas <nicolas.delfino@tretton37.com>
2019-12-08 21:07:32 +0100

    3rd party and stuff

---
+++ features/products/react/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios("http://localhost:4000/items/v1/").then(result => {
+        console.log("res", result);
+        setData(result.data);
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {data.map(item => (
+        <li key={item.objectID}>
+          <img src={`${item.image}`} alt={item.title} />
+          <div className="title">{item.title}</div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const rootElement = document.getElementById("react-root");
+
+ReactDOM.render(<App />, rootElement);
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
-const express = require('express')
-const router = express.Router() // eslint-disable-line
+const data = require("./data.json");
+const express = require("express");
+const path = require("path");
+const router = express.Router() // eslint-disab
 
-router.get('/', (req, res) => {
-  res.render('slow')
-})
+router.get("/", (req, res) => {
+  res.render("slow");
+});
 
-router.get('/medium', (req, res) => {
-  res.render('medium')
-})
+router.get("/medium", (req, res) => {
+  res.render("medium");
+});
 
-router.get('/fast', (req, res) => {
-  res.render('fast')
-})
+router.get("/fast", (req, res) => {
+  res.render("fast");
+});
 
-module.exports = router
+router.get("/items/v1", (req, res) => {
+  res.json(data);
+});
+
+router.get("/gtm", (req, res) => {
+  res.sendFile('facebook.js', { root: path.join(__dirname, '../public/js/vendors') });
+});
+
+module.exports = router;
